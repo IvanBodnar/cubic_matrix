7274df869b1dc59337aacbfd07080075ddbec475
ivanbodnar <ibodnar@outlook.com>
2019-06-04 19:03:07 -0300

    Test if all positions of the matrix are initialized at zero

--- cubic_matrix/test_matrix.py
+++ cubic_matrix/test_matrix.py
@@ -5,6 +5,6 @@ class TestCubicMatrix:
 
     def test_creates(self):
         cm = CubicMatrix(3, 3, 2)
-        assert len(cm.matrix) == 18
+        assert len([x for x in cm.matrix.values() if x == 0]) == 18
         assert cm.matrix.get((1, 1, 1)) == 0
         assert cm.matrix.get((3, 3, 2)) == 0

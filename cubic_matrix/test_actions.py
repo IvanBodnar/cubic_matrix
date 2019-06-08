cda2b85992012410d16160f2e8147ed4a499d657
ivanbodnar <ibodnar@outlook.com>
2019-06-08 09:01:16 -0300

    Tests added for QueryAction class

--- cubic_matrix/test_actions.py
+++ cubic_matrix/test_actions.py
@@ -1,5 +1,5 @@
 from cubic_matrix.matrix import CubicMatrix
-from cubic_matrix.actions import UpdateAction
+from cubic_matrix.actions import UpdateAction, QueryAction
 
 
 class TestUpdateAction:
@@ -16,3 +16,13 @@ class TestUpdateAction:
         update = UpdateAction([1, 1, 1], cm.matrix)
         update.execute()
         assert cm.matrix.get((1, 1, 1)) == 1
+
+
+class TestQueryAction:
+
+    def test_creates(self):
+        cm = CubicMatrix(3, 3, 2)
+        query = QueryAction([1, 1, 1, 3, 1, 2], cm.matrix)
+        assert query._start_position == (1, 1, 1)
+        assert query._end_position == (3, 1, 2)
+        assert query._matrix is cm.matrix
